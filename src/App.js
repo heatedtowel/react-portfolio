358b34b07c2305a7a0d85f31a3a3a5b702d5db0b
Jesse Monks <monksjesse@gmail.com>
2022-03-28 01:48:02 -0400

    added framer for animation, added animation and transitions to toggle button, added animation to about me section. Need to change transition but initial work is started

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ import Footer from './components/footer/footer'
 function App() {
 
   const [currentTab, setCurrentTab] = useState('About Me');
-  const [theme, setCurrentTheme] = useState('App--dark');
+  const [theme, setCurrentTheme] = useState(true);
 
   const updateTab = (tab) => {
     setCurrentTab(tab)
   }
 
-  const updateTheme = (theme) => {
-    setCurrentTheme(theme)
+  const updateTheme = () => {
+    setCurrentTheme(!theme)
   }
 
   return (
-    <div className={theme}>
+    <div className='App' data-theme={theme}>
       <header>
-        <Header updateTab={updateTab} updateTheme={updateTheme} />
+        <Header updateTab={updateTab} updateTheme={updateTheme} theme={theme} />
       </header>
       <section className="d-flex container min-vh-100 justify-items-center">
         {currentTab === 'Projects' ? <RepoList /> : null}
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,16 @@
 import Navigation from "../navbar/navigation";
-import { Navbar, Container, Nav, Button, ButtonGroup } from "react-bootstrap";
-import './assets/css/style.css'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { motion } from 'framer-motion'
+import './assets/css/header.css'
 import Resume from './assets/Resume.pdf'
-import { FaRegLightbulb, FaLightbulb } from "react-icons/fa";
 
+export default function Header({ theme, updateTab, updateTheme }) {
 
-export default function Header({ updateTab, updateTheme }) {
+  const spring = {
+    type: "spring",
+    stiffness: 200,
+    damping: 15,
+  };
 
   return (
     <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
@@ -14,13 +19,12 @@ export default function Header({ updateTab, updateTheme }) {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Navigation updateTab={updateTab} updateTheme={updateTheme} />
+            <Navigation updateTab={updateTab} />
             <Nav.Link className='navLinks' key='resume' href={Resume}>Resume</Nav.Link>
           </Nav>
-          <ButtonGroup aria-label="Basic example" className="themeBtn">
-            <Button size="sm" onClick={() => updateTheme('App--light')} variant="light"> <FaLightbulb /> </Button>
-            <Button size="sm" onClick={() => updateTheme('App--dark')} variant="dark"> <FaRegLightbulb /> </Button>
-          </ButtonGroup>
+          <div className="switch" data-isOn={theme} onClick={() => updateTheme()}>
+            <motion.div className="handle" data-isOn={theme} layout transition={spring} />
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
